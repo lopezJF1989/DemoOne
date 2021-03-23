aa145b541563b62fbbc91ea94f18afbbf06ff39e
lopez <lopez@LAPTOP-H4LQEVT9>
2021-03-23 14:06:09 -0400

    3/23/21 second commit

--- src/demo2/GithubDemoTwo.java
+++ src/demo2/GithubDemoTwo.java
@@ -3,6 +3,8 @@ package demo2;
 public class GithubDemoTwo {
 	public static void main(String[]args) {
 		System.out.println("GitHub Demo 2 My first Github commit");
+		
+		System.out.println("My second GitHub commit");
 	}
 
 }
